776746c78ecaf3a93747c17f376f799c5c52bd3f
Farhana <farhanafatema@hotmail.com>
2020-12-13 21:56:06 +0000

    hopefully it will get deployed now!

--- src/Statement.js
+++ src/Statement.js
@@ -16,7 +16,7 @@ function Statement(){
             <Col md={8}>
             <div className="statement">
             A confident, social-media savvy and business-minded individual, I relish any opportunity to talk about and promote what I’m passionate about, either through blogging or public speaking.  
-            As a graduate of <a href= 'https://codeyourfuture.io/' target= '_blank'>CodeYourFuture’s</a> Full Stack Web Development programme, and following on from a wealth of experience in business and communications, I have a skill-set ranging from front-end frameworks like React and Bootstrap, back-end technologies like Node.js to database management in postreSQL. I’m seeking a position in a fast-paced environment where I can put my web development skills to use and keep learning every day.
+            As a graduate of <a href= 'https://codeyourfuture.io/'>CodeYourFuture’s</a> Full Stack Web Development programme, and following on from a wealth of experience in business and communications, I have a skill-set ranging from front-end frameworks like React and Bootstrap, back-end technologies like Node.js to database management in postreSQL. I’m seeking a position in a fast-paced environment where I can put my web development skills to use and keep learning every day.
             </div>
             </Col>
             <Col sm={2}></Col>
