f73ec75aa6998a9619f48fd9d1b87b561e5bc993
Farhana <farhanafatema@hotmail.com>
2020-12-15 19:55:51 +0000

    typo fixed

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(){
     return(
         <div className= "header">
-            <p>Farhana Fatima Khan | Apiring Full Stack Developer</p>
+            <p>Farhana Fatima Khan | Aspiring Full Stack Developer</p>
         </div>
     )
 
